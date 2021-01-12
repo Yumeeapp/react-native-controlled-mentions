17f910da31cf40ef0f8ea475622e751bf0faecf3
Philip Pisa <svarto@protonmail.com>
2021-01-12 10:09:12 +0100

    added in ReplyMessageBox option

--- src/components/mention-input.tsx
+++ src/components/mention-input.tsx
@@ -1,47 +1,48 @@
-import React, { FC, MutableRefObject, useMemo, useRef, useState } from 'react';
+import React, { FC, MutableRefObject, useMemo, useRef, useState } from "react";
 import {
   NativeSyntheticEvent,
   Text,
   TextInput,
   TextInputSelectionChangeEventData,
   View,
-} from 'react-native';
+} from "react-native";
 
-import { MentionInputProps, MentionPartType, Suggestion } from '../types';
+import { MentionInputProps, MentionPartType, Suggestion } from "../types";
 import {
   defaultMentionTextStyle,
   generateValueFromPartsAndChangedText,
   generateValueWithAddedSuggestion,
   isMentionPartType,
   parseValue,
-} from '../utils';
+} from "../utils";
 
-const MentionInput: FC<MentionInputProps> = (
-  {
-    value,
-    onChange,
+const MentionInput: FC<MentionInputProps> = ({
+  value,
+  onChange,
 
-    partTypes = [],
+  partTypes = [],
+  ReplyMessageBox,
 
-    inputRef: propInputRef,
+  inputRef: propInputRef,
 
-    containerStyle,
+  containerStyle,
 
-    onSelectionChange,
+  onSelectionChange,
 
-    ...textInputProps
-  },
-) => {
+  ...textInputProps
+}) => {
   const textInput = useRef<TextInput | null>(null);
 
-  const [selection, setSelection] = useState({start: 0, end: 0});
+  const [selection, setSelection] = useState({ start: 0, end: 0 });
 
-  const {
-    plainText,
-    parts,
-  } = useMemo(() => parseValue(value, partTypes), [value, partTypes]);
+  const { plainText, parts } = useMemo(() => parseValue(value, partTypes), [
+    value,
+    partTypes,
+  ]);
 
-  const handleSelectionChange = (event: NativeSyntheticEvent<TextInputSelectionChangeEventData>) => {
+  const handleSelectionChange = (
+    event: NativeSyntheticEvent<TextInputSelectionChangeEventData>
+  ) => {
     setSelection(event.nativeEvent.selection);
 
     onSelectionChange && onSelectionChange(event);
@@ -53,7 +54,9 @@ const MentionInput: FC<MentionInputProps> = (
    * @param changedText
    */
   const onChangeInput = (changedText: string) => {
-    onChange(generateValueFromPartsAndChangedText(parts, plainText, changedText));
+    onChange(
+      generateValueFromPartsAndChangedText(parts, plainText, changedText)
+    );
   };
 
   /**
@@ -69,7 +72,9 @@ const MentionInput: FC<MentionInputProps> = (
    * 'abc @|name dfg' - keyword is against ''
    * 'abc @name |dfg' - keyword is against undefined'
    */
-  const keywordByTrigger = useMemo((): { [trigger: string]: string | undefined } => {
+  const keywordByTrigger = useMemo((): {
+    [trigger: string]: string | undefined;
+  } => {
     const newKeywordByTrigger: { [trigger: string]: string | undefined } = {};
 
     partTypes.filter(isMentionPartType).forEach((mentionPartType) => {
@@ -77,13 +82,23 @@ const MentionInput: FC<MentionInputProps> = (
         return undefined;
       }
 
-      if (parts.some(one => one.data != null && selection.end >= one.position.start && selection.end <= one.position.end)) {
+      if (
+        parts.some(
+          (one) =>
+            one.data != null &&
+            selection.end >= one.position.start &&
+            selection.end <= one.position.end
+        )
+      ) {
         return undefined;
       }
 
-      const triggerIndex = plainText.lastIndexOf(mentionPartType.trigger, selection.end);
-      const spaceIndex = plainText.lastIndexOf(' ', selection.end - 1);
-      const newLineIndex = plainText.lastIndexOf('\n', selection.end - 1);
+      const triggerIndex = plainText.lastIndexOf(
+        mentionPartType.trigger,
+        selection.end
+      );
+      const spaceIndex = plainText.lastIndexOf(" ", selection.end - 1);
+      const newLineIndex = plainText.lastIndexOf("\n", selection.end - 1);
 
       switch (true) {
         case triggerIndex == -1:
@@ -105,7 +120,7 @@ const MentionInput: FC<MentionInputProps> = (
         case spaceIndex + 1 === triggerIndex:
           newKeywordByTrigger[mentionPartType.trigger] = plainText.substring(
             triggerIndex + 1,
-            selection.end,
+            selection.end
           );
       }
     });
@@ -118,13 +133,15 @@ const MentionInput: FC<MentionInputProps> = (
    * - Get updated value
    * - Trigger onChange callback with new value
    */
-  const onSuggestionPress = (mentionType: MentionPartType) => (suggestion: Suggestion) => {
+  const onSuggestionPress = (mentionType: MentionPartType) => (
+    suggestion: Suggestion
+  ) => {
     const newValue = generateValueWithAddedSuggestion(
       parts,
       mentionType,
       plainText,
       selection,
-      suggestion,
+      suggestion
     );
 
     if (!newValue) {
@@ -151,51 +168,54 @@ const MentionInput: FC<MentionInputProps> = (
     textInput.current = ref as TextInput;
 
     if (propInputRef) {
-      if (typeof propInputRef === 'function') {
+      if (typeof propInputRef === "function") {
         propInputRef(ref);
       } else {
-        (propInputRef as MutableRefObject<TextInput>).current = ref as TextInput;
+        (propInputRef as MutableRefObject<
+          TextInput
+        >).current = ref as TextInput;
       }
     }
   };
 
   return (
     <View style={containerStyle}>
-      {(partTypes
-        .filter(one => isMentionPartType(one) && one.renderSuggestions != null) as MentionPartType[])
-        .map((mentionType) => (
-          <React.Fragment key={mentionType.trigger}>
-            {mentionType.renderSuggestions && mentionType.renderSuggestions({
+      {(partTypes.filter(
+        (one) => isMentionPartType(one) && one.renderSuggestions != null
+      ) as MentionPartType[]).map((mentionType) => (
+        <React.Fragment key={mentionType.trigger}>
+          {mentionType.renderSuggestions &&
+            mentionType.renderSuggestions({
               keyword: keywordByTrigger[mentionType.trigger],
               onSuggestionPress: onSuggestionPress(mentionType),
             })}
-          </React.Fragment>
-        ))
-      }
-
-      <TextInput
-        {...textInputProps}
-
-        ref={handleTextInputRef}
-
-        multiline
-
-        onChangeText={onChangeInput}
-        onSelectionChange={handleSelectionChange}
-      >
-        <Text>
-          {parts.map(({text, partType, data}, index) => partType ? (
-            <Text
-              key={`${index}-${data?.trigger ?? 'pattern'}`}
-              style={partType.textStyle ?? defaultMentionTextStyle}
-            >
-              {text}
-            </Text>
-          ) : (
-            <Text key={index}>{text}</Text>
-          ))}
-        </Text>
-      </TextInput>
+        </React.Fragment>
+      ))}
+      <View>
+        <ReplyMessageBox />
+        <TextInput
+          {...textInputProps}
+          ref={handleTextInputRef}
+          multiline
+          onChangeText={onChangeInput}
+          onSelectionChange={handleSelectionChange}
+        >
+          <Text>
+            {parts.map(({ text, partType, data }, index) =>
+              partType ? (
+                <Text
+                  key={`${index}-${data?.trigger ?? "pattern"}`}
+                  style={partType.textStyle ?? defaultMentionTextStyle}
+                >
+                  {text}
+                </Text>
+              ) : (
+                <Text key={index}>{text}</Text>
+              )
+            )}
+          </Text>
+        </TextInput>
+      </View>
     </View>
   );
 };
